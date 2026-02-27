3bfcfff165a62714b169c8a5f1509d017833efbe
Owen Pettiford <owenpettiford@Owens-MacBook-Air.local>
2026-02-27 22:24:31 +0000

    Disable browser caching for game files

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3001;
-app.use(express.static(path.join(__dirname, 'public')));
-app.listen(PORT, () => console.log(`Asteroids running at http://localhost:${PORT}`));
+app.use(express.static(path.join(__dirname, "public"), { etag: false, maxAge: 0 }));
+app.listen(PORT, () => console.log(`Astroheads running at http://localhost:${PORT}`));
